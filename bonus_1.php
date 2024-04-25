f8ae9bd7d9fcb54f43e6f5bb6788b3506c69ab42
sheikhjaber0424 <sheikhjaber190@gmail.com>
2024-04-25 16:49:24 +0600

    bonus-1 done

---
+++ bonus_1.php
@@ -0,0 +1,53 @@
+<?php
+
+// Define a function that we'll need to use later
+// This function works exactly the same way as the "min" function, except it returns the INDEX of the value which is minimum, not the VALUE of the value which is minimum
+function findMinIndex($array)
+{
+    $minIndex = null;
+    $minValue = null;
+    foreach ($array as $i => $value) {
+        if ($minValue === null || $value < $minValue) {
+            $minIndex = $i;
+            $minValue = $value;
+        }
+    }
+    return $minIndex;
+}
+
+function calculateTime($q, $tapFlows, $walkingTime)
+{
+    $tapTimes = array_fill(0, count($tapFlows), 0);
+    $val = 0;
+
+    foreach ($q as $i => $bottleSize) {
+
+        $minIndex = findMinIndex($tapTimes);
+
+        $flow = $tapFlows[$minIndex];
+
+        $timeSpentFillingBottle = $bottleSize / $flow;
+
+        $val = $val + 1;
+
+        if ($val > count($tapFlows)) {
+            $tapTimes[$minIndex] += $timeSpentFillingBottle + $walkingTime;
+        } else {
+            $tapTimes[$minIndex] += $timeSpentFillingBottle;
+        }
+    }
+
+    $totalTime = max($tapTimes);
+
+    return $totalTime;
+}
+
+$queueExample = array(400, 750, 1000);
+$walkTimeExample = 5;
+$flowRatesExample = [50, 200];
+
+echo '-----';
+echo "\n";
+echo calculateTime($queueExample, $flowRatesExample, $walkTimeExample);
+echo "\n";
+echo '-----';
